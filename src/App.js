97cc4ab4643174afb02513d3a01de2c73e2077e4
Alexandre <alexandreeduardolemes@gmail.com>
2022-05-05 23:49:19 -0300

    Adiciona top header e algumas regras de estilização

--- src/App.js
+++ src/App.js
@@ -5,8 +5,49 @@ import '../src/Styles/App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <div className="side-header">
+        <Header />
+      </div>
+      <div className="side-content">
+        <div className="top-header">
+          <div className="top-title">
+            <p>
+              Anais do Simpósio Latino Americano de Ciências de Alimentos
+            </p>
+            <h5>
+            Anais do 13 Simpósio Latino Americano de Ciências de Alimentos
+            </h5>
+            <p>
+              ISSN: 1234-5678
+            </p>
+          </div>
+
+
+          <select className="select-header">
+            <option>
+              <span>🌐</span>PT, BR
+            </option>
+            <option>
+              <span>🌐</span>EN, US
+            </option>
+            <option>
+              <span>🌐</span>EN, UK
+            </option>
+          </select>
+
+          <div className="user-info-header">
+            <p>Bem Vindo!</p>
+            <p>alguem12@galoascience.com</p>
+            
+            <img
+              src="https://media.istockphoto.com/vectors/user-icon-people-icon-isolated-on-white-background-vector-vector-id1210939712?k=20&m=1210939712&s=612x612&w=0&h=xJqEPQnMiNofprbLXWdEtJQ75QL79lQ5N76J4JOdTIM="
+              alt="Profile Pic"
+              className="profile-pic"
+            />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
