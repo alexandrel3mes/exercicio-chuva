e19e9f414d8648c6a154b34f5010cb909cfd84da
Alexandre <alexandreeduardolemes@gmail.com>
2022-05-09 20:53:53 -0300

    Form feito e abertura de novva discussão inicaida

--- src/Components/DiscussionCard.js
+++ src/Components/DiscussionCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Styles/DiscussionCard.css';
 
 import handWithPlant from '../Images/handWithPlant.png';
@@ -7,11 +7,19 @@ import qAndA from '../Images/qAndA.png';
 import QuestionCard from "./QuestionCard";
 
 function DiscussionCard() {
+  const [createMode, setCreateMode] = useState(false);
+  const [endMode, setEndMode] = useState(false)
+
+  const endForm = () => {
+    setCreateMode(false);
+    setEndMode(true);
+  }
   return (
     <div className="discussion">
       <p className="discussion-title">Discussões</p>
 
       <div className="discussion-body">
+        { !createMode && !endMode && (
         <div className="discussion-header">
           <p className="header-share">Compartilhe suas ideias ou dúvidas com os autores!</p>
 
@@ -22,8 +30,39 @@ function DiscussionCard() {
           </div>
 
           <p className="header-text">Sabia que o maior estímulo no desenvolvimento científico e cultural é a curiosidade? Deixe seus questionamentos ou sugestões para o autor!</p>
-          <button className="create-btn">+ criar tópico</button>
+          <button onClick={() => setCreateMode(true) } className="create-btn">+ criar tópico</button>
         </div>
+        )
+      }
+        { createMode && (
+          <div className="create-mode">
+            <p className="form-header">Tem uma dúvida ou sugestão? Compartilhe seu feedback com os autores!</p>
+            <form className="form">
+              <label className="form-label" htmlFor="subject">Assunto <br />
+                <input
+                  className="text-input"
+                  id="subject"
+                  type="text"
+                  placeholder="Defina um tópico sucinto para notificar os autores..."
+                />
+              </label> <br />
+              <label className="form-label" htmlFor="content">Conteúdo <br />
+                <textarea
+                  className="area-input"
+                  id="content"
+                />
+              </label>
+            </form>
+            <button onClick={ () => endForm() } className="form-btn">Enviar</button>
+          </div>
+        )
+      }
+
+      { endMode && (
+        <div>
+          <h1>Teste</h1>
+        </div>
+      )}
 
         <QuestionCard />
         <QuestionCard />
