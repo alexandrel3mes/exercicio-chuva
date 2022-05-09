cb017f789f16d6980e49c0bddb0a714f8ca060fa
Alexandre <alexandreeduardolemes@gmail.com>
2022-05-09 17:53:45 -0300

    Primeira section pronta

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import '../src/Styles/App.css';
 
 import SideMenu from './Components/SideMenu';
 import Header from './Components/Header';
+import DetailCard from './Components/DetailCard';
 
 function App() {
   return (
@@ -12,8 +13,38 @@ function App() {
         <Header />
 
         <main className="main">
-        </main>
+          <section className="first-sec">
+            <div className="title-and-video">
+              <p className="p-title">
+              Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP 
+              </p>
+              <iframe
+                className='video'
+                width="560"
+                height="315"
+                src="https://www.youtube-nocookie.com/embed/OoKhpnDuc4k"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+              </iframe>
+
+            </div>
+
+            <div className="btns-and-details">
+              <div>
+                <button className="first-sec-btn">Download</button>
+                <button className="first-sec-btn">Star</button>
+                <button className="first-sec-btn">Icon</button>
+
+                <p className="how-to-quote">COMO CITAR ESSE TRABALHO?</p>
+              </div>
+
+              <DetailCard />
+            </div>
+          </section>
         <Footer />
+        </main>
     </div>
   );
 }
--- src/Components/DetailCard.js
+++ src/Components/DetailCard.js
@@ -1,21 +1,32 @@
 import React from "react";
+import '../Styles/DetailCard.css';
 
 function DetailCard() {
   return(
-    <div>
-      <p>Detalhes</p>
-      <p>Tipo de Apresentação: <strong>Pôster</strong></p>
-      <p>Eixo temático: <strong>Alimentação e saúde A5</strong></p>
-      <p>Palavras-chaves: <strong>Alimentos funcionais, alimentação escolar.</strong></p>
+    <div className="detail-card">
+      <p className="detail-title">Detalhes</p>
 
-      <p>
-        <strong>Autores:</strong> <br />
-        Galileo Galilei¹ <br />
-        Berta Lange de Morretes² <br />
-        Isaac Newton³ <br />
-        Cesar Lattes¹ <br />
-        Stephen Hawking⁴ <br />
-      </p>
+      <div className="detail-description">
+        <p className="detail-p tags">Tipo de Apresentação: <strong>Pôster</strong></p>
+        <p className="detail-p tags">Eixo temático: <strong>Alimentação e saúde A5</strong></p>
+        <p className="detail-p tags">Palavras-chaves: <strong>Alimentos funcionais, alimentação escolar.</strong></p>
+
+        <p className="detail-p quote">
+          <strong>Autores:</strong> <br />
+          Galileo Galilei¹ <br />
+          Berta Lange de Morretes² <br />
+          Isaac Newton³ <br />
+          Cesar Lattes¹ <br />
+          Stephen Hawking⁴ <br />
+        </p>
+        
+        <p className="detail-p quote-detail">
+          ¹Universidade Estadual de Campinas <br />
+          ²Universidade de São Paulo <br />
+          ³Instituto Nacional de Pesquisas Espaciais <br />
+          ⁴Universidade Federal do Rio de Janeiro <br />
+        </p>
+      </div>
     </div>
   )
 }
